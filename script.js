e640f5b4a500b6af144a3a66f48b0a79cd721ce6
Josh Hurn <42974930+joshua-hurn@users.noreply.github.com>
2020-02-03 23:31:56 -0600

    finished

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var friends = ['Bart', 'Homer', 'Lisa', 'Marge', 'Maggie'];
+    var locations = ['bathroom', 'kitchen', 'foyer', 'dining room', 'office', 'library', 'sun room', 'shed', 'garden', 'driveway'];
+    var weapons = ['mace', 'sword', 'candlestick', 'rope', 'club', 'laptop', 'icepick', 'saber', 'knife', 'bedpost', 'gun', 'rifle', 'trebuchet', 'cannon', 'laser', 'machine gun', 'bullet', 'fist', 'airplane', 'Earth'];
+    
+    function addDetails(i) {
+        return function () {
+            alert(`Accusation ${i}: I accuse ${friends[i % 20]}, with the ${weapons[i % 5]} in the ${locations[i % 10]}!`);
+        }
+    }
+
+    for (var i = 1; i < 101; i++) {
+        var h3 = document.createElement('h3');
+        h3.textContent = `Accusation ${i}`;
+        let details = addDetails(i);
+        h3.addEventListener('click', details);
+        document.body.appendChild(h3);
+    }
+});
